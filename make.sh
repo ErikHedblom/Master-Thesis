4a03142d3deaefdcf9b05ba9cc8d93edbda2fbb8
Erik Hedblom <Hedblom.e@gmail.com>
2017-06-01 14:10:54 +0200

    Stated explaining rule 4

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 dot -Teps Dot/DotAccess.dot -o EPS-graphs/DotAccess.eps
 dot -Teps Dot/Parent.dot -o EPS-graphs/Parent.eps
+dot -Teps Dot/Component.dot -o EPS-graphs/Component.eps
 
 pdflatex report
 bibtex report
