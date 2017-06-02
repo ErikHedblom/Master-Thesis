c4b1d2fb5963e5a434d7775d37d31bcad2c4db59
Erik Hedblom <Hedblom.e@gmail.com>
2017-06-02 11:08:36 +0200

    Added example for unresolvanle accesses

--- make.sh
+++ make.sh
@@ -2,6 +2,7 @@
 dot -Teps Dot/DotAccess.dot -o EPS-graphs/DotAccess.eps
 dot -Teps Dot/Parent.dot -o EPS-graphs/Parent.eps
 dot -Teps Dot/Component.dot -o EPS-graphs/Component.eps
+dot -Teps Dot/BrokenAccess.dot -o EPS-graphs/BrokenAccess.eps
 
 pdflatex report
 bibtex report
